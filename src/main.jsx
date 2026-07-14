6a3c782bf28cddbdc167e8e420033240dd7ddfb4
mfdcolecttion <matheusduraes3532@gmail.com>
2026-07-13 21:13:28 -0300

    Corrige router para GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import App from "./App.jsx";
 import Home from "./pages/Home.jsx";
@@ -10,52 +10,38 @@ import Search from "./pages/Search.jsx";
 
 import "./index.css";
 
-
 createRoot(document.getElementById("root")).render(
-
   <StrictMode>
-
-    <BrowserRouter>
+    <HashRouter>
 
       <Routes>
 
-
         <Route element={<App />}>
-
 
           <Route 
             path="/" 
             element={<Home />} 
           />
 
-
           <Route 
             path="/movie/:id" 
             element={<MovieDetails />} 
           />
-
 
           <Route 
             path="/tv/:id" 
             element={<SeriesDetails />} 
           />
 
-
           <Route 
             path="/search" 
             element={<Search />} 
           />
 
-
         </Route>
-
 
       </Routes>
 
-
-    </BrowserRouter>
-
-
+    </HashRouter>
   </StrictMode>
-
 );
